c40b7f1c9646a5765ec662a5b7f50a45ef9ec430
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-04-14 16:50:09 -0600

    Se agrega Plug.Cowboy para ser supervisado

--- lib/ejemplo_app/application.ex
+++ lib/ejemplo_app/application.ex
@@ -8,8 +8,8 @@ defmodule EjemploApp.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: EjemploApp.Worker.start_link(arg)
-      # {EjemploApp.Worker, arg}
+      {EjemploApp.Hola, :ok},
+      {Plug.Cowboy, scheme: :http, plug: EjemploApp.Endpoint, options: [port: 80]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
